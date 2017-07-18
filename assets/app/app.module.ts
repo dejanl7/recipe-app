ece451ef0111373e15c69cde67ed73753506c837
Dex <dejanloncar907@gmail.com>
2017-07-18 13:04:24 +0200

    Basic components added

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from "./app.component";
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from "@angular/http";
 
+//import { routing } from "./app.routing";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { ShopComponent } from './shop/shop.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
+    declarations: [
+        AppComponent, 
+        HeaderComponent, 
+        HomeComponent, 
+        ProfilesComponent, 
+        ShopComponent, 
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        //routing
+    ],
     bootstrap: [AppComponent]
 })
+
 
 export class AppModule {
 
