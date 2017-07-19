7b3acac8b77d6455fb8c7f6254f05069d6fc237b
Dex <dejanloncar907@gmail.com>
2017-07-19 11:11:33 +0200

    Users model created

---
+++ models/users.js
@@ -0,0 +1,57 @@
+var mongoose        = require('mongoose');
+var Schema          = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
+
+// Blueprint for Users Table
+var schema = new Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String
+    },
+    profileImage: {
+        type: String
+    },
+    dateCreated: {
+        type: Date,
+        default: Data.now(),
+        required: true
+    },
+    dateUpdated: {
+        type: Date,
+        default: Data.now()
+    },
+    userRecipes: {
+        type: Schema.Types.ObjectId,
+        ref: 'Recipes'
+    }
+});
+
+
+// Plugin for checking unique records
+schema.plugin(uniqueValidator);
+
+
+
+module.exports = mongoose.model(
+    'Users',
+    schema
+)
